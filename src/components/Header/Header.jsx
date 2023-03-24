79789b973b94e809123e997c2aeb4726d9e589c4
unknown <denmichigan123@gmail.com>
2023-03-24 14:26:21 +0100

    Finished version of the Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import logo from "./Images/Whitecroft.svg"
+import HeaderCss from "./Header.module.css"
+
+const header = HeaderCss.header
+const headerWrapper = HeaderCss.headerWrapper
+const workBtn = HeaderCss.workBtn
+
+
+const Header = () =>{
+    return(
+        <header className={header}>
+            <div className={headerWrapper}>
+            <img src={logo} alt="" />
+                <nav className="">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about">About us</a>
+                        </li>
+                        <li>
+                            <a href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a href="#portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#clients">Clients</a>
+                        </li>
+                    </ul>
+                    <button className={workBtn}>Work with us</button>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
